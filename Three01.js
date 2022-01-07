5d9c2a8a1f63eaaabd5f3254674cd3137a3606ae
andreoliveiraalves96 <andreoliveiraalves96@gmail.com>
2022-01-07 12:27:38 +0000

    fewij

--- Three01.js
+++ Three01.js
@@ -1,5 +1,8 @@
 import * as THREE from './libs/three.module.js';
-let camera, scene, renderer, geometry, material, flagArrowLeft = false, flagArrowRight = false; 
+let camera, scene, renderer, geometry, material, flagArrowLeft = false, flagArrowRight = false, flagArrowUp = false, jumpInterval, leftInterval, rightInterval
+let goingUp = false, goingLeft = false, goingRight = false
+let cx = 0; let cy = -20; let cz = 10;
+const posY = [-10, 0, 10]
 // once everything is loaded, we run our Three.js stuff
 
 window.onload = function init() {
@@ -12,9 +15,9 @@ window.onload = function init() {
 
     camera = new THREE.PerspectiveCamera(75, aspect, 0.1, 1000); // perspective camera
 
-    camera.position.x = 0
-    camera.position.y = -20;
-    camera.position.z = 10 // place the camera using world coordinates
+    camera.position.x = cx
+    camera.position.y = cy;
+    camera.position.z = cz // place the camera using world coordinates
     camera.lookAt(scene.position); //point the camera to the center of the scene
 
     // create a renderer: if no Canvas parameter is passed, a new canvas element will be created
@@ -28,14 +31,14 @@ window.onload = function init() {
     geometry = new THREE.BoxGeometry(2, 2, 2);
     material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
     const cube = new THREE.Mesh(geometry, material);
-    cube.position.y = -10
+    cube.position.y = cy + 10;
     scene.add(cube);
 
 
     // immediately use the texture for material creation
     const texture = new THREE.TextureLoader().load('textures/floor.jpg.jpg');
 
-    geometry = new THREE.PlaneGeometry(50, 3000);
+    geometry = new THREE.PlaneGeometry(50, 20000);
     material = new THREE.MeshBasicMaterial({ map: texture })
     const plane = new THREE.Mesh(geometry, material);
     plane.position.x = 0;
@@ -47,7 +50,7 @@ window.onload = function init() {
     document.onkeydown = function (e) {
         switch (e.key) {
             case 'ArrowUp':
-                // up arrow
+                flagArrowUp = true;
                 break;
             case 'ArrowDown':
                 // down arrow
@@ -61,29 +64,59 @@ window.onload = function init() {
         }
     };
 
+    function jump() {
+        if (cube.position.z > 8) {
+            goingUp = false;
+        }
+        if (goingUp)
+            cube.position.z += 0.2;
+        if (!goingUp) {
+            cube.position.z -= 0.2;
+            if(cube.position.z < 0){
+                clearInterval(jumpInterval);
+            }
+        }
+    }
+
+    function turnLeft() {
+        if (cube.position.x > -10){
+            cube.position.x -= 0.3
+        }
+        if(cube.position.x <= -10){
+        clearInterval(leftInterval);
+        }
+    }
+
+    function turnRight() {
+        if (cube.position.x < 10){
+            cube.position.x += 0.3
+        }
+        if(cube.position.x >= 10){
+        clearInterval(rightInterval);
+        }
+    }
 
     function render() {
-        console.log(plane.position)
 
-        plane.position.y--;
-
-        if (plane.position.y < -2500) {
-            plane.position.y = 2500
-        }
+        cube.position.y++;
+        camera.position.y++;
 
         if (flagArrowLeft) {
-            cube.position.x -= 0.25;
-            if (cube.position.x <= -10) {
-                flagArrowLeft = false
-            }
+            flagArrowLeft = false;
+            goingLeft = true;
+            leftInterval = setInterval(turnLeft,15);
         }
-        
-        
+
         if (flagArrowRight) {
-            cube.position.x += 0.25;
-            if (cube.position.x >= 10) {
-                flagArrowRight = false
-            }
+            flagArrowRight = false;
+            goingRight = true;
+            rightInterval = setInterval(turnRight,15);
+
+        }
+        if (flagArrowUp) {
+            flagArrowUp = false;
+            goingUp = true;
+            jumpInterval = setInterval(jump, 15)
         }
 
         renderer.render(scene, camera);
